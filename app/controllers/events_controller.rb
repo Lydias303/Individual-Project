e1309c0ab473e5f1aa18478ce5249f088028ad07
Lydias303 <shaferlydia@yahoo.com>
2015-03-17 23:14:18 -0600

    Have it all working

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,68 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Events.all
+
+    art = params[:artist_name]
+    if params[:artist_name].nil?
+      @events = Event.all
+    else
+      events = EventServices.events(artist_name: art)
+      parsed = JSON.parse(events)
+      parsed_events = parsed["resultsPage"]["results"]["event"]
+      parsed_events.each do |event|
+        created_event = Event.create(
+          display_name: event["displayName"],
+          event_type:   event["type"],
+          lat:          event["venue"]["lat"],
+          lng:          event["venue"]["lng"],
+          datetime:     event["start"]["date"]
+        )
+
+        created_event.venue = Venue.create(
+          display_name: event["venue"]["displayName"],
+          lat:          event["venue"]["lat"],
+          lng:          event["venue"]["lng"],
+          city:         event["venue"]["metroArea"]["displayName"],
+          country:      event["venue"]["metroArea"]["country"]["displayName"]
+        )
+
+        event["performance"].each do |artist|
+          artist = Artist.create(
+            display_name: artist["displayName"]
+          )
+
+          ArtistsEvent.create(artist_id: artist.id, event_id: created_event.id)
+        end
+      end
+
+      @events = Event.artist(params[:artist_name].downcase)
+      require 'pry' ; binding.pry
+    end
   end
 end
 
-# require 'faraday'
-# key = "RknuC79GGhjfBGWp"
-# conn = Faraday.new
-# artist  = "weezer"
-# response = conn.get "http://api.songkick.com/api/3.0/search/artists.json?query=#{artist}&apikey=#{key}"
-# puts response.body
-
-
+#
+# def index
+#   @events = EventServices.where(nil)
+#   artist = Artist.includes(:events).find_by(display_name: params[:display_name])
+#   if artist
+#     @events << artist.events
+#   end
+#   filtering_params(params).each do |key, value|
+#     @events = @events.public_send(key, value) if value.present?
+#   end
+# end
+#
+# def show
+#   @event = Event.find_by(id: params[:id])
+# end
+#
+# private
+#
+# def filtering_params(params)
+#   params.slice(:display_name, :city, :state, :venue, :date, :artists, :event_type)
+# end
+#end
 
 
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ActiveRecord::Base
   has_many :users, through: :events_users
 
   belongs_to :venue
-  
+
   scope :city,         -> (city)  { where("city ILIKE ?", city) }
   scope :state,        -> (state) { where("state ILIKE ?", state) }
   scope :display_name, -> (display_name) { where("display_name ILIKE ?", display_name) }
@@ -16,16 +16,4 @@ class Event < ActiveRecord::Base
     joins(:artists_events => :artist).where('artists.display_name' => artist_name)
   }
   scope :date_range, -> (start_date, end_date){where datetime: start_date..end_date }
-
-  def self.service
-    @service ||= EventServices.new
-  end
-
-  def self.all(artist)
-    get_artist_calender(artist)
-  end
-
-  def self.event_search(options = {})
-    EventServices.events(options)
-  end
 end
