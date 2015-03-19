9eb1509ddf5949a90640380af376f0c1dec7697d
Lydias303 <shaferlydia@yahoo.com>
2015-03-19 10:04:56 -0600

    trying to fix geocoder

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,40 @@
+Geocoder.configure(
+
+:lookup => bing, :api_key => "Al-ruqwG-2ZvGkKS7gjDDBj7HPUg-mts3ZBkmzJDYEzJXeDT8vjrHC5QFeSXYZq9",
+
+  # :use_https=> true,
+
+  :timeout=> 1
+  )
+
+
+# Geocoder.configure(
+#
+#   :api_key=> "AIzaSyCen0qZ8OEjFdrDjh4ZEgayYh81gPgyc-c",
+#
+
+#
+#   :lookup=>:google,
+#
+#   # :ip_lookup=>:telize,
+#
+#   :language=>:en,
+#
+#   :http_headers=>{},
+#
+  # :use_https=> true
+  # )
+#
+#   :http_proxy=> nil,
+#
+#   :https_proxy=> nil,
+#
+#   :cache=>nil,
+#
+#   :cache_prefix=>"geocoder:",
+#   #
+#   # :units=>:km,
+#   #
+#   # :distances=>:linear
+#
+# )
