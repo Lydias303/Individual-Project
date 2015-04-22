f3b772dbe7fd14d6d35c1a7a41b19cb1a33b8bb1
Lydias303 <shaferlydia@yahoo.com>
2015-04-22 15:24:35 -0600

    cleaned up tests

--- spec/feature/user_spec.rb
+++ spec/feature/user_spec.rb
@@ -11,14 +11,4 @@ describe "a logged in user can" do
   let!(:user) do
     User.create(provider: "facebook2", name: "dana", email: "dana@gmail.com", uid: "12345", oauth_token: "12345")
   end
-
-  # it "can see their logout button" do
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).
-  #     and_return(:user)
-  #
-  #   expect(page).to have_content("Log Out")
-  #   expect(page).to have_content("Likes")
-  #   expect(current_page).to eq(root_path)
-  # end
-
 end
